844d1a7254d1eb27bc2abbc33bb52f3958131ad0
Jos Boumans <jos@dwim.org>
2020-04-21 13:48:57 -0700

    * first commit

---
+++ js/game-head.js
@@ -0,0 +1,73 @@
+    <title>Spelling Bee - The New York Times</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui" />
+    <meta http-equiv="Content-Type" content="text/html" charSet="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="version" content="v6.5.0" />
+    <meta name="CG" content="Crosswords/Games" />
+    <meta name="PT" content="spelling bee" />
+    <link rel="manifest" href="/games-assets/v2/metadata/sb-web-manifest.json?v=v6.5.0" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/games-assets/v2/metadata/sb-apple-touch-icon.png?v=v6.5.0" />
+    <link rel="mask-icon" color="#f7cc32" href="/games-assets/v2/metadata/sb-safari-pinned-tab.svg?v=v6.5.0" />
+    <link rel="shortcut icon" href="/games-assets/v2/metadata/sb-favicon.ico?v=v6.5.0" />
+    <meta name="apple-mobile-web-app-title" content="Spelling Bee" />
+    <meta name="application-name" content="Spelling Bee" />
+    <meta name="msapplication-TileColor" content="#f7cc32" />
+    <meta name="theme-color" content="#f7cc32" />
+    <meta property="og:title" content="The New York Times Spelling Bee" />
+    <meta property="og:url" content="https://www.nytimes.com/puzzles/spelling-bee" />
+    <meta property="og:description" content="Play the daily spelling bee" />
+    <meta property="og:image" content="https://static01.nyt.com/packages/other/crossword/fb-og/purple.png" />
+    <style id="responsive.css">
+        #ratio-hook,
+        #width-hook {
+            position: absolute;
+            top: -100em;
+            -webkit-transition: width 0.001ms;
+            transition: width 0.001ms
+        }
+
+        @media (max-width: 443.98px) {
+            #width-hook {
+                width: 0px
+            }
+        }
+
+        @media (min-width: 444px) and (max-width: 767.98px) {
+            #width-hook {
+                width: 1px
+            }
+        }
+
+        @media (min-width: 768px) and (max-width: 991.98px) {
+            #width-hook {
+                width: 2px
+            }
+        }
+
+        @media (min-width: 992px) and (max-width: 1199.98px) {
+            #width-hook {
+                width: 3px
+            }
+        }
+
+        @media (min-width: 1200px) {
+            #width-hook {
+                width: 4px
+            }
+        }
+
+        @media (orientation: portrait) {
+            #ratio-hook {
+                width: 0px
+            }
+        }
+
+        @media (orientation: landscape) {
+            #ratio-hook {
+                width: 1px
+            }
+        }
+    </style>
+    <link rel="stylesheet" type="text/css" href="/games-assets/v2/foundation.0cb215c4813b7d667be14dd10a393bd5.css" />
+    <link rel="stylesheet" type="text/css" href="/games-assets/v2/foundation-game.1255567f6ff83b0ef9ffc1ce4102d145.css" />
+    <link rel="stylesheet" type="text/css" href="/games-assets/v2/spelling-bee.9508e7e2b1c321ebe7013329708e6f39.css" />
